73d257d3f49e101cbcfef9c488afe176a4cde016
AnshikaN <95581274+AnshikaN@users.noreply.github.com>
2024-07-16 22:58:11 +0530

    Time: 2 ms (24.29%), Space: 42.3 MB (17.08%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(String[] logs) {
+         Stack<String> stack = new Stack<>();
+        for(int i=0;i<logs.length;i++){
+            if(!logs[i].equals("../")&& !logs[i].equals("./") )
+            stack.push(logs[i]);
+            else if (logs[i].equals("../")&&!stack.isEmpty())
+            stack.pop();
+        }
+        return stack.size();
+
+    }
+}
